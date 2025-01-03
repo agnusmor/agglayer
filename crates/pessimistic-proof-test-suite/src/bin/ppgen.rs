e8b49911a82b4c80fd0eed87d39e47bb1dc796a3
agnusmor <agnusmor@gmail.com>
2025-01-03 12:43:35 +0100

    Modify ppgen to generate input file for Zisk

--- crates/pessimistic-proof-test-suite/src/bin/ppgen.rs
+++ crates/pessimistic-proof-test-suite/src/bin/ppgen.rs
@@ -3,8 +3,7 @@ use std::{path::PathBuf, time::Instant};
 use agglayer_types::{Certificate, U256};
 use clap::Parser;
 use pessimistic_proof::{
-    bridge_exit::{NetworkId, TokenInfo},
-    PessimisticProofOutput,
+    bridge_exit::{NetworkId, TokenInfo}, LocalNetworkState, PessimisticProofOutput
 };
 use pessimistic_proof_test_suite::{
     runner::Runner,
@@ -15,6 +14,8 @@ use serde::{Deserialize, Serialize};
 use sp1_sdk::HashableKey;
 use tracing::{info, warn};
 use uuid::Uuid;
+pub type Hasher = pessimistic_proof::local_exit_tree::hasher::Keccak256Hasher;
+pub type MultiBatchHeader = pessimistic_proof::multi_batch_header::MultiBatchHeader<Hasher>;
 
 /// The arguments for the pp generator.
 #[derive(Parser, Debug)]
@@ -25,7 +26,7 @@ struct PPGenArgs {
     n_exits: usize,
 
     /// The number of imported bridge exits.
-    #[clap(long, default_value = "0")]
+    #[clap(long, default_value = "10")]
     n_imported_exits: usize,
 
     /// The optional output directory to write the proofs in JSON. If not set,
@@ -54,8 +55,16 @@ fn get_events(n: usize, path: Option<PathBuf>) -> Vec<(TokenInfo, U256)> {
     }
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+struct PessimisticProofInput {
+    state: LocalNetworkState,
+    batch_header: MultiBatchHeader,
+}
+
 pub fn main() {
     sp1_sdk::utils::setup_logger();
+
+    info!("Starting the pessimistic proof generator");
 
     let args = PPGenArgs::parse();
 
@@ -85,45 +94,54 @@ pub fn main() {
         imported_bridge_exits.len()
     );
 
-    let start = Instant::now();
-    let (proof, vk, new_roots) = Runner::new()
-        .generate_plonk_proof(&old_state.into(), &multi_batch_header)
-        .expect("proving failed");
-    let duration = start.elapsed();
-    info!(
-        "Successfully generated the plonk proof with a latency of {:?}",
-        duration
-    );
-
-    let vkey = vk.bytes32().to_string();
-    info!("vkey: {}", vkey);
-
-    let fixture = PessimisticProofFixture {
-        certificate,
-        pp_inputs: new_roots.into(),
-        signer: state.get_signer(),
-        vkey: vkey.clone(),
-        public_values: format!("0x{}", hex::encode(proof.public_values.as_slice())),
-        proof: format!("0x{}", hex::encode(proof.bytes())),
+    let zisk_input = PessimisticProofInput {
+        state: old_state.clone().into(),
+        batch_header: multi_batch_header.clone(),
     };
 
-    if let Some(proof_dir) = args.proof_dir {
-        // Save the plonk proof to a json file.
-        let proof_path = proof_dir.join(format!(
-            "{}-exits-v{}-{}.json",
-            args.n_exits,
-            &vkey[..8],
-            Uuid::new_v4()
-        ));
-        if let Err(e) = std::fs::create_dir_all(&proof_dir) {
-            warn!("Failed to create directory: {e}");
-        }
-        info!("Writing the proof to {:?}", proof_path);
-        std::fs::write(proof_path, serde_json::to_string_pretty(&fixture).unwrap())
-            .expect("failed to write fixture");
-    } else {
-        info!("Proof: {:?}", fixture);
-    }
+    let mut pp_file = std::fs::File::create("pp_input.bin").unwrap();
+    bincode::serialize_into(&mut pp_file, &zisk_input).unwrap();    
+
+    info!("pp_input.bin created");
+    // let start = Instant::now();
+    // let (proof, vk, new_roots) = Runner::new()
+    //     .generate_plonk_proof(&old_state.into(), &multi_batch_header)
+    //     .expect("proving failed");
+    // let duration = start.elapsed();
+    // info!(
+    //     "Successfully generated the plonk proof with a latency of {:?}",
+    //     duration
+    // );
+
+    // let vkey = vk.bytes32().to_string();
+    // info!("vkey: {}", vkey);
+
+    // let fixture = PessimisticProofFixture {
+    //     certificate,
+    //     pp_inputs: new_roots.into(),
+    //     signer: state.get_signer(),
+    //     vkey: vkey.clone(),
+    //     public_values: format!("0x{}", hex::encode(proof.public_values.as_slice())),
+    //     proof: format!("0x{}", hex::encode(proof.bytes())),
+    // };
+
+    // if let Some(proof_dir) = args.proof_dir {
+    //     // Save the plonk proof to a json file.
+    //     let proof_path = proof_dir.join(format!(
+    //         "{}-exits-v{}-{}.json",
+    //         args.n_exits,
+    //         &vkey[..8],
+    //         Uuid::new_v4()
+    //     ));
+    //     if let Err(e) = std::fs::create_dir_all(&proof_dir) {
+    //         warn!("Failed to create directory: {e}");
+    //     }
+    //     info!("Writing the proof to {:?}", proof_path);
+    //     std::fs::write(proof_path, serde_json::to_string_pretty(&fixture).unwrap())
+    //         .expect("failed to write fixture");
+    // } else {
+    //     info!("Proof: {:?}", fixture);
+    // }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
